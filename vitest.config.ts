3b0fc6a451edb7c1b830cf31a6945cf1e622c6c1
karakblondal <karakblondal@gmail.com>
2025-04-15 22:58:07 -0400

    playwright óvart 2x

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
+    // Exclude e2e tests and node_modules so Vitest won’t pick them up
+    exclude: ['e2e/**/*', '**/node_modules/**']
   },
-}); 
+});
